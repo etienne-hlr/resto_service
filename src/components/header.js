a8c2b0fc915686f43281dcc69fb835b62517ae9b
etienne <etienne.haller@epitech.eu>
2023-04-01 12:38:29 +0200

    adding bootstrap and creating navbar header

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBarHeader() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#carte">Carte</Nav.Link>
+            <Nav.Link href="#commandes">Commandes</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBarHeader;
