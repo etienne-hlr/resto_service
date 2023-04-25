2887f26f8b183bc203b21db320594d81422379ba
etienne <etienne.haller@epitech.eu>
2023-04-25 16:29:57 +0200

    designing first list

--- src/components/list_command.js
+++ src/components/list_command.js
@@ -1,13 +1,23 @@
 import React from "react";
-
+import { ListGroup, Button, Col, Row } from "react-bootstrap";
 const CommandList = ({ items }) => {
-  console.log(items);
   return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
+    <Row className="d-flex justify-content-center pt-2">
+      <Col sm={10} xs={10} md={8}>
+        <ListGroup as="ul">
+          {items.map((item, index) => (
+            <ListGroup.Item
+              key={index}
+              as="li"
+              className="d-flex justify-content-between align-items-center"
+            >
+              <p>{item}</p>
+              <Button variant="outline-danger">Supprimer</Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
+    </Row>
   );
 };
 
