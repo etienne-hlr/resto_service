b2d7869484b4c4c68ec57318adcf18a8ba3b6283
etienne <etienne.haller@epitech.eu>
2023-04-28 16:22:19 +0200

    modyfing home

--- src/pages/Carte.js
+++ src/pages/Carte.js
@@ -1,22 +1,29 @@
 import React from "react";
 import CardItemCarte from "../components/card_item_carte";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { InputGroup, Row, Col, FormControl } from "react-bootstrap";
 
 function Carte() {
   return (
-    <Row xs={1} sm={2} md={4} className="g-3 p-3">
-      {Array.from({ length: 8 }).map((_, index) => (
-        <Col key={index}>
-          <CardItemCarte
-            key={index}
-            image="https://www.socialkitchen.fr/photos/logo-thefork.jpg"
-            title="Titre de la boisson"
-            data="data"
-          />
-        </Col>
-      ))}
-    </Row>
+    <div>
+      <InputGroup>
+        {/* <InputGroup.Text> */}
+        <FormControl type="search" placeholder="Rechercher" />
+        {/* </InputGroup.Text> */}
+      </InputGroup>
+      <p>Ajouter</p>
+      <Row xs={1} sm={2} md={4} className="g-3 p-3">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <Col key={index}>
+            <CardItemCarte
+              key={index}
+              image="https://www.socialkitchen.fr/photos/logo-thefork.jpg"
+              title="Titre de la boisson"
+              data="data"
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
   );
 }
 
