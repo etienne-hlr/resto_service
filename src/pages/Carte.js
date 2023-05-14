7c8eaae772f39b349326963f7645ff22899a1ed6
etienne <etienne.haller@epitech.eu>
2023-05-14 23:58:03 +0200

    some css

--- src/pages/Carte.js
+++ src/pages/Carte.js
@@ -144,7 +144,7 @@ function Carte() {
               Ajouter à la carte
             </Button>
           </div>
-          <InputGroup className="w-25 h-25">
+          <InputGroup style={{ width: "200px" }} className="h-25 ">
             <FormControl type="search" placeholder="Rechercher" />
             <InputGroup.Text>
               <svg
@@ -152,7 +152,7 @@ function Carte() {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-search"
+                className="bi bi-search"
                 viewBox="0 0 16 16"
               >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
