686b8726d05fb1728b6beb457b81a7b1c9faee7b
etienne <etienne.haller@epitech.eu>
2023-06-21 00:26:52 +0200

    redirection working !

--- back/routes/api/index.js
+++ back/routes/api/index.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const apiUsers = require("./users");
 
 router.use("/users", apiUsers);
+router.get("/test", (req, res) => {
+  res.json("ok !");
+});
 
 module.exports = router;
