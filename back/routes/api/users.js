126cca099ea37b6793a3ebf40dc5a83fb7fdce96
etienne <etienne.haller@epitech.eu>
2023-06-23 16:17:16 +0200

    added bcrypt

--- back/routes/api/users.js
+++ back/routes/api/users.js
@@ -1,15 +1,24 @@
 const UserModel = require("../../database/models/user_model");
-
+const bcrypt = require("bcrypt");
 const router = require("express").Router();
 
 router.post("/", async (req, res) => {
-  const body = req.body;
-  const newUser = new UserModel(body);
+  const { name, email, password } = req.body;
   try {
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
     const user = await newUser.save();
     res.json(user);
   } catch (error) {
-    res.status(400).json("error user creation");
+    res.status(400).json(error);
   }
 });
 
