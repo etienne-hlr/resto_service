a98c788f26ef5d2772b077eac42aea37b290b3d8
etienne <etienne.haller@epitech.eu>
2023-06-23 17:05:07 +0200

    refactoring app

--- back/routes/api/index.js
+++ back/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const apiUsers = require("./users");
+const apiSignup = require("./signup");
 
-router.use("/users", apiUsers);
+router.use("/signup", apiSignup);
 
 module.exports = router;
