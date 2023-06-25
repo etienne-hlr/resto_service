d39b1338bf9d743249d3dc06cc1045d5d1906359
etienne <etienne.haller@epitech.eu>
2023-06-26 00:11:00 +0200

    signin rdy

--- front/src/pages/Authentication/Signin.js
+++ front/src/pages/Authentication/Signin.js
@@ -7,27 +7,49 @@ import { useNavigate } from "react-router-dom";
 import createUser from "../../api/authentication/signup";
 
 function Signin() {
+  const validationSchema = yup.object({
+    email: yup.string().required("Email manquant").email("Email invalide"),
+    password: yup
+      .string()
+      .required("Mot de passe manquant")
+      .min(6, "Le mot de passe doit faire au minimum 6 caractères"),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const submit = handleSubmit((credentials) => {
+    console.log(credentials);
+  });
+
   return (
     <div className="d-flex justify-content-center p-5">
       <div className="w-15"></div>
       <div className="border rounded p-4 shadow">
         <h2>Se connecter</h2>
-        <Form>
-          <Form.Group className="mb-3" controlId="formUsername">
-            <Form.Label>Nom d'utilisateur</Form.Label>
+        <Form onSubmit={submit}>
+          <Form.Group className="mb-3" controlId="SignInUserEmail">
+            <Form.Label>Votre email</Form.Label>
             <Form.Control
               className="shadow-sm"
               type="username"
               placeholder="Entrez le nom d'utilisateur"
+              {...register("email")}
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Group className="mb-3" controlId="SignInUserPassword">
             <Form.Label>Mot de passe</Form.Label>
             <Form.Control
               className="shadow-sm"
               type="password"
               placeholder="Entrez un mot de passe"
+              {...register("password")}
             />
           </Form.Group>
 
