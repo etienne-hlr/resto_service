2da6df3c0998233965b38e5873831222cec6ca39
etienne <etienne.haller@epitech.eu>
2023-07-04 01:08:23 +0200

    adapted axios for setting cookie

--- front/src/api/authentication/signin.js
+++ front/src/api/authentication/signin.js
@@ -1,11 +1,12 @@
 import instanceAxios from "../instance";
 
-const signin = async (loginUser) => {
+const signin = async (signin) => {
+  console.log(signin);
   try {
-    let response = await instanceAxios.post("/signin", {
-      email: loginUser.email,
-      password: loginUser.password,
+    let response = await instanceAxios.post("/signin", signin, {
+      withCredentials: true,
     });
+    console.log(response);
   } catch (error) {
     console.log(error);
   }
